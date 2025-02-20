9bb30d7416d7199adda01c487a9d98d4b70ec913
grig <grig_p@mail.ru>
2025-02-20 10:48:07 +0300

    Картинка удаления теста

--- lib/features/results_screen/view/results_screen.dart
+++ lib/features/results_screen/view/results_screen.dart
@@ -73,17 +73,50 @@ class _ResultsScreenState extends State<ResultsScreen> {
             ),
           ],
         ),
-        // body: BlocBuilder<ResultBloc, ResultState>(
-        //   bloc: _database,
-        //   builder: (context, state) {
-        //     if (state is DataLoaded) {
-        //       // List<DataBlock> data = _zeroing(state.data);
-        //       // var kfr = KfrCalculator(data);
-        //       return Center();
-        //     }
-        //   },
-        // ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: _onNewTest,
+              heroTag: 'NewTest',
+              tooltip: 'Провести тест',
+              foregroundColor: Theme.of(context).colorScheme.primary,
+              backgroundColor: Theme.of(context).colorScheme.surface,
+              child: Image.asset('lib/assets/icons/play48.png'),
+            ),
+            const SizedBox(width: 40),
+            FloatingActionButton(
+              onPressed: _onDeleteTest,
+              heroTag: 'DeleteTest',
+              tooltip: 'Удалить тест',
+              foregroundColor: Theme.of(context).colorScheme.primary,
+              backgroundColor: Theme.of(context).colorScheme.surface,
+              child: Image.asset('lib/assets/icons/delete48.png'),
+            ),
+            const SizedBox(width: 40),
+            FloatingActionButton(
+              onPressed: _onOpenTest,
+              heroTag: 'OpenTest',
+              tooltip: 'Открыть тест',
+              foregroundColor: Theme.of(context).colorScheme.primary,
+              backgroundColor: Theme.of(context).colorScheme.surface,
+              child: Image.asset('lib/assets/icons/res_test48.png'),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void _onNewTest() {
+
+  }
+
+  void _onDeleteTest() {
+
+  }
+
+  void _onOpenTest() {
+
   }
 }
